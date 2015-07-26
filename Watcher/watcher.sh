4d6535dea1076474419e82e3a82e24cab65a0d6d
Sam Quinn <quinnsam1@gmail.com>
2015-07-26 14:25:43 -0700

    Import from bin

---
+++ Watcher/watcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+tail -fn0 /var/log/auth.log | \
+    while read line ; do
+        print_connected=$(echo "$line" | grep "Accepted")
+        if [ $? = 0 ]
+        then
+            echo "$print_connected" | pb
+            echo "$print_connected" >> /home/squinn/logs/SSH_connections.log
+
+        fi
+    done
