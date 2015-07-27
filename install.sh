dfdbf5db834cc8ef2c88891f4c6c5fe1f6176a19
Sam Quinn <quinnsam1@gmail.com>
2015-07-27 08:51:15 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,39 @@
 #! /usr/bin/zsh
-
+PWD=`pwd`
+UHOME=$HOME
 
 if [ ! -d $HOME/bin ]; then
+    echo "Creating Home bin"
     mkdir $HOME/bin
 fi
 
 ################################################################################
 #                                Trash install
 ################################################################################
+echo "Installing Trash [dr]"
 ln -s -t $HOME/bin `pwd`/Trash/dr
 ln -s -t $HOME/bin `pwd`/Trash/cleanup.sh
 
 ################################################################################
 #                                Fixvnc install
 ################################################################################
+echo "Installing Fixvnc"
 ln -s -t $HOME/bin `pwd`/Fixvnc/fixvnc
 
 ################################################################################
 #                                Watcher install
 ################################################################################
+echo "Installing Watcher"
 ln -s -t $HOME/bin `pwd`/Watcher/watcher.sh
+
+cat /etc/crontab | grep watcher.sh > /dev/null 2> $HOME/logs/Bin_install.log
+if [ $? ]; then
+    echo "installing Watcher in crontab"
+    sudo echo "*/5 * * * * root $PWD/Watcher/watcher.sh 2> $UHOME/logs/Watcher.log" >> /etc/crontab
+fi 
 
 ################################################################################
 #                                PhoneHome install
 ################################################################################
+echo "Installing Phonehome"
 ln -s -t $HOME/bin `pwd`/PhoneHome/PhoneHome.sh
